c2ca0b53849a3451e72159fe9205dac183b745b5
twopheat <twopheat@gmail.com>
2020-07-12 14:43:22 -0700

    added bkg placeholder until more is built in.

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <body>
-       <Main />
-      </body> */}
+      <body>
+       {/* <Main /> */}
+      </body>
       <Footer />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,16 +15,16 @@ function Nav() {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto1">
       <li className="nav-item active">
-        <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/about">About</a>
+        <a className="nav-link" href="#">About</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/portfolio">Portfolio</a>
+        <a className="nav-link" href="#">Portfolio</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/contact">Contact</a>
+        <a className="nav-link" href="#">Contact</a>
       </li>
     </ul>
   </div>
