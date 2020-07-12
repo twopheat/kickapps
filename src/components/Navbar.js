fe24fee2e454cd1e488fff7281dee4a2924f41d4
twopheat <twopheat@gmail.com>
2020-07-12 16:25:18 -0700

    fixed build routes via htaccess

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from "react";
 import logo from "../../src/logo.png";
-//import logo from '../logo.svg';
-//import Router from '../routes/route';
 import "../App.css";
 
 function Nav() {
