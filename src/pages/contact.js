9e750fbb34b927fa2d4fbbcce1f0129ff5b7eb12
twopheat <twopheat@gmail.com>
2020-07-13 10:21:15 -0700

    all but contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,19 +12,23 @@ class Contact extends Component {
       <div className="mt-5 container">
         
         <h5 className="ptitle text-dark">Contact Us</h5>
+        <br></br>
         <div className="row">
           <div className="col-sm">
             <h4 className="riverside">riverside,ca</h4>
             </div>
+            <br></br>
             <div className="col-sm">
             <h4 className="denver">denver,co</h4>
             </div>
+            <br></br>
             <div className="col-sm">
             <h4 className="nashville">nashville,tn</h4>
+            <br></br>
           </div>
         </div>
         <h5 className="email">info@kickapps.com</h5>
-        
+        <br></br>
         <h4 className="phone">951.956.6966</h4>
 
 
