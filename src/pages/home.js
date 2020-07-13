afec5f73ca34f162df68304b4fe197337c85c9c7
twopheat <twopheat@gmail.com>
2020-07-12 22:57:16 -0700

    stylized logo cards

--- src/pages/home.js
+++ src/pages/home.js
@@ -19,7 +19,7 @@ class Home extends Component {
       <div className="container">
         <div className="row">
 
-          <div className="col-4-sm sloganbox">
+          <div className="col-4-sm sloganbox mt-5">
             <div className="row">
               <div className="firstletter">D</div><div className="slogan">igitally</div>
             </div>
@@ -30,16 +30,16 @@ class Home extends Component {
               <div className="firstletter">D</div><div className="slogan">evelopment</div>
             </div>
           </div>
-          <div className="col-1 dividerbox align-middle">
+          <div className="col-1 dividerbox">
             <img className="homepic divider" src={divider} alt="KICKapps"></img>
           </div>
-          <div className="col-7 logobox align-middle">
+          <div className="col-7 logobox mt-5">
             <img className="homepic" src={logo} alt="KICKapps"></img>
           </div>
         </div>
         <div className="row justify-content-center mt-5">
           <div className="col-5-sm toolsbox1">
-            <div className="card round shadow-sm ">
+            <div className="card round nodecard shadow-sm ">
               <img className="nodepic" src={nodepic} alt="Node.js"></img>
             </div>
           </div>
@@ -49,13 +49,13 @@ class Home extends Component {
           <div className="col-7-sm toolsbox2">
             <div className="row justify-content-center">
               <div className="col-5-sm align-middle">
-                <div className="card round shadow-sm m-2 mt-5">
+                <div className="card round expresscard shadow m-2 mt-5">
                   <img className="expresspic" src={expresspic} alt="Express.js"></img>
                 </div>
               </div>
 
               <div className="col-5-sm">
-                <div className="card round shadow-sm mt-3">
+                <div className="card round reactcard shadow-lg mt-3">
                   <img className="reactpic" src={reactpic} alt="React.js"></img>
                 </div>
               </div>
@@ -63,13 +63,13 @@ class Home extends Component {
 
             <div className="row justify-content-center">
               <div className="col-5-sm">
-                <div className="card round shadow-sm m-2">
+                <div className="card round mysqlcard shadow m-2">
                   <img className="mysqlpic" src={mysqlpic} alt="mySQL"></img>
                 </div>
               </div>
 
               <div className="col-5-sm">
-                <div className="card round shadow-sm mt-3">
+                <div className="card round mongocard shadow-sm mt-3">
                   <img className="mongopic" src={mongopic} alt="MongoDB"></img>
                 </div>
               </div>
