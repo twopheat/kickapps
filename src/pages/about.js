394627e7e87278a564b4ba7d56009a3e198b701c
twopheat <twopheat@gmail.com>
2020-07-16 14:30:09 -0700

    more style and layout tweaks

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-import aboutpic from "../../src/img/about.png";
+import aboutpic from "../../src/img/coding.jpg";
 import divider from "../../src/img/dividervert.png";
 
 class About extends Component {
@@ -18,21 +18,23 @@ class About extends Component {
     return (
 
       <div className="mt-5 mb-5 container">
-        <div className="row justify-content-center">
-          <div className="col-5-sm leftcol mb-4">
+        <div id="textkey" className="row justify-content-center">
+         
+          <div className="col-5-sm leftcol ztop mb-4">
             <h5>We are a team of coders</h5><br></br><h5>spanning the entire US.</h5>
           </div>
-          <div className="col-1 dividerbox">
+          <div className="col-1 dividerbox ztop">
               <img className="divider2" src={divider} alt="KICKapps"></img>
             </div>
             <br></br>
-          <div className="col-5-sm rightcol mb-4">
+          <div id="righttextkey" className="col-5-sm rightcol ztop mb-4">
               <h5>We have superior availabilty</h5><br></br><h5>and a simple, swift flow.</h5>
           </div>
-            <div className="card round col-10 aboutcard shadow-sm mb-5">
+      
+        </div>
+        <div id="aboutkey" className="card aboutcard round col-10 mb-5">
               <img className="aboutpic" src={aboutpic} alt="KICKapps Mission"></img>
             </div>
-        </div>
       </div>
 
 
