924252909e415a1b7cc1aa3c7be91532a56fb819
twopheat <twopheat@gmail.com>
2020-07-18 15:42:24 -0700

    mailto link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,16 +6,19 @@ import colopic from "../../src/img/colo.png";
 import tennpic from "../../src/img/tenn.png";
 
 class Contact extends Component {
-  
+
   render() {
 
     return (
 
       <div className="mt-5 mb-5 container">
-        
+
         <h5 className="ptitle text-dark">Contact Us</h5>
         <br></br>
-        <h5 className="email">info@kickapps.com</h5>
+        <h5 className="email">
+          <a href="mailto:info@kickapps.com">info@kickapps.com</a>
+        </h5>
+
         <br></br>
         <h4 className="phone">951.956.6966</h4>
         <br></br>
@@ -23,15 +26,15 @@ class Contact extends Component {
           <div className="col-sm">
             <img className="calipic" src={calipic} alt="cali"></img>
             <h4 className="riverside">riverside,ca</h4>
-            </div>
-            <br></br>
-            <div className="col-sm">
+          </div>
+          <br></br>
+          <div className="col-sm">
             <img className="colopic" src={colopic} alt="colo"></img>
             <h4 className="denver">denver,co</h4>
-            </div>
-            <br></br>
-            <div className="col-sm">
-            <img className="tennpic" src={tennpic} alt="tenn"></img>  
+          </div>
+          <br></br>
+          <div className="col-sm">
+            <img className="tennpic" src={tennpic} alt="tenn"></img>
             <h4 className="nashville">nashville,tn</h4>
             <br></br>
           </div>
@@ -39,9 +42,9 @@ class Contact extends Component {
 
 
 
-      
 
-        
+
+
       </div>
     );
   }
