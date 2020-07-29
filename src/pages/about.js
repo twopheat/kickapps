d8257a9bd32672b355f61add0cc825155b85b7c4
twopheat <twopheat@gmail.com>
2020-07-28 20:38:20 -0700

    about text color

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,7 +21,7 @@ class About extends Component {
         <div id="textkey" className="row justify-content-center">
          
           <div className="col-5-sm m-3 text-center ztop mb-4">
-            <h5 className="text-white">Here at KICKapps we house a team of specialized programmers and business development professionals. Our streamlined build process and diverse skill-sets can help achieve your digital identity.</h5>
+            <h5>Here at KICKapps we house a team of specialized programmers and business development professionals. Our streamlined build process and diverse skill-sets can help achieve your digital identity.</h5>
           </div>
 
       
