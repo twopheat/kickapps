ce25a91cb25c9fe0e1258739f869fc5584cb3f03
twopheat <twopheat@gmail.com>
2020-07-29 21:12:34 -0700

    nav ans css adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import "../App.css";
 function Nav() {
   return (
 <nav className="navbar navbar-expand-lg navbar-light bg-kick shadow-sm">
-  <img src={logo} className="Applogo ml-3" alt="logo" href="#"/>
+  <a href="/"><img src={logo} className="Applogo ml-3" alt="logo" /></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
