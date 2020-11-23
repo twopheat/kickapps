e596b9a14fdbf4f27af9ceec48ff643c2726d2ed
jmo5896 <justin.m.moore5896@gmail.com>
2020-11-23 15:47:11 -0600

    pre-pull

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-import logo from "../../src/img/octopimplogo.png";
+import logo from "../../src/img/octopimpLOGO.png";
 import nodepic from "../../src/img/node.png";
 import expresspic from "../../src/img/express.png";
 import reactpic from "../../src/img/react.png";
